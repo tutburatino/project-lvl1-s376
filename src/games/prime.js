f1e914b8cabc7dfd2d38581924749f4ac7ca9946
mike <tutburatino@gmail.com>
2018-11-13 21:51:02 +0300

    Corrected isProme for nagative numbers.

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,6 +5,7 @@ import { cons } from 'hexlet-pairs';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
+  if (num < 0) { return false; }
   for (let i = 2; i < num; i += 1) { if (num % i === 0) return false; }
   return num !== 1 && num !== 0;
 };
