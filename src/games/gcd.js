64f04707a02619835889b15d273f2e89808f721d
mike <tutburatino@gmail.com>
2018-11-13 14:04:12 +0300

    Corrected function name.

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,19 +4,19 @@ import { cons } from 'hexlet-pairs';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
-const getGreatestDivisor = (a, b) => {
+const greatestCommonDivisor = (a, b) => {
   const greatest = a >= b ? a : b;
   const smallest = a < b ? a : b;
   if (smallest === 0) { return 0; }
   const rest = greatest - smallest;
-  return rest === 0 ? smallest : getGreatestDivisor(rest, smallest);
+  return rest === 0 ? smallest : greatestCommonDivisor(rest, smallest);
 };
 
 const generator = () => {
   const a = randomInt(10);
   const b = randomInt(10);
   const question = `${a} ${b}`;
-  const rightAnswer = `${getGreatestDivisor(a, b)}`;
+  const rightAnswer = `${greatestCommonDivisor(a, b)}`;
   return cons(question, rightAnswer);
 };
 
