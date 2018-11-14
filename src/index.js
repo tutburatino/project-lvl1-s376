d9a3945fc13f9b36dfcb1fde402f3d65a2e0e67d
mike <tutburatino@gmail.com>
2018-11-14 21:00:46 +0300

    Deleted returns.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 import readlineSync from 'readline-sync';
 import { car, cdr } from 'hexlet-pairs';
 
@@ -13,22 +14,23 @@ const numberOfSteps = 3;
 
 export const makeGame = (description, generator) => {
   const userName = initGame(description);
-
   const playGame = (step) => {
     if (step <= 0) {
-      return console.log(`Congratulations, ${userName}!`);
+      console.log(`Congratulations, ${userName}!`);
     }
-    const exercise = generator();
-    const question = car(exercise);
-    console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    const rightAnswer = cdr(exercise);
-    if (!(userAnswer === rightAnswer)) {
+    if (step > 0) {
+      const exercise = generator();
+      const question = car(exercise);
+      console.log(`Question: ${question}`);
+      const userAnswer = readlineSync.question('Your answer: ');
+      const rightAnswer = cdr(exercise);
+      if (userAnswer === rightAnswer) {
+        console.log('Correct!');
+        return playGame(step - 1);
+      }
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
-      return console.log(`Let's try again, ${userName}!`);
+      console.log(`Let's try again, ${userName}!`);
     }
-    console.log('Correct!');
-    return playGame(step - 1);
   };
   playGame(numberOfSteps);
 };
